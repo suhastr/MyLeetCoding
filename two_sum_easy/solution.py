99a4cd49b68c3874fa59524b5b9633b784d04ac8
suhas t r <36911828+suhastr@users.noreply.github.com>
2023-02-28 22:26:30 +0530

    Update solution.py

--- two_sum_easy/solution.py
+++ two_sum_easy/solution.py
@@ -1,5 +1,7 @@
+# problem: https://leetcode.com/problems/two-sum
+
 class Solution:
-    # Two sum
+    
     def twoSum(self, nums: List[int], target: int) -> List[int]:
             for i in range(len(nums)):
                 for j in range(i+1,len(nums)):
